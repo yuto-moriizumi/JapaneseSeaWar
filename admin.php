7be2021e1561481f9794359671532baf7fe81978
VolgaKurvar <kurvan1112@gmail.com>
2018-09-25 00:15:54 +0900

    no message

---
+++ admin.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(0);
+mb_language("ja");
+mb_internal_encoding('UTF-8');
+
+//JSから受信
+$url=$_POST["data"];
+$command = explode(",",$_POST["data"],2); //命令取り出し
+
+if( strcmp($command[0], "addProvince") == 0 ){ //マップクリック処理
+    $fp = fopen('provinces.csv', 'a');
+    fwrite($fp,$command[1].",\n");
+    fclose($fp);
+    print "Success";
+}else{
+    print "Failed";
+}
+?>
