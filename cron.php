25710648dddff4d559e000e943187c0fa0b21015
VolgaKurvar <kurvan1112@gmail.com>
2018-10-14 00:50:47 +0900

    マップ範囲を拡大 新規国家：明の追加 マップ色塗りアルゴリズムを改良

--- cron.php
+++ cron.php
@@ -17,7 +17,7 @@ for($i=0; empty($filec[$i])===false;$i++){
 
 //マスから資金を回収
 for($i=0; empty($filep[$i])===false;$i++){
-    if($filep[$i][1]==$filep[$i][2]){ //領有国と管理国が同じだったら
+    if(strcmp($filep[$i][1], $filep[$i][2]) == 0){ //領有国と管理国が同じだったら
         for($j=0; empty($filec[$j])===false;$j++){
             if($filec[$j][0]==$filep[$i][1]){ //領有国を探して
                 $filec[$j][2]=$filec[$j][2]+1; //1円を追加
@@ -26,7 +26,17 @@ for($i=0; empty($filep[$i])===false;$i++){
     }
 }
 
+
+//一次元配列に変換
+for($i=0; empty($filep[$i])===false;$i++){
+    $filep[$i]=implode(",",$filep[$i]);
+}
+for($i=0; empty($filec[$i])===false;$i++){
+    $filec[$i]=implode(",",$filec[$i]);
+}
+
 //CSV出力
-//いんぷろーどすること
-file_put_contents("test.csv",$filec);
+file_put_contents("provinces.csv",$filep);
+file_put_contents("countries.csv",$filec);
+
 ?>
